3d189de898d2d525eab08dc236b78ab1b077d6b9
GJasonAnderson <31811561+GJasonAnderson@users.noreply.github.com>
2018-11-30 09:41:55 -0500

    correct host home deadline

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,6 +1,6 @@
 // COUNTDOWN TIMER
 // Set the date we are counting down to
-var countDownDate = new Date("November 30, 2018 00:00:00").getTime();
+var countDownDate = new Date("November 30, 2018 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
